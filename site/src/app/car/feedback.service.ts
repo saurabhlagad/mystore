cba7af2d2a697f44f3d9f1978874eb0d1c87980a
Aniket2703 <aniketnimbalkar270320@gmail.com>
2020-12-07 11:41:14 +0530

    Feedback

--- site/src/app/car/feedback.service.ts
+++ site/src/app/car/feedback.service.ts
@@ -1,9 +1,37 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FeedbackService {
+  
+  constructor(private http:HttpClient) { }
 
-  constructor() { }
+  url='http://localhost:4100/feedback'
+
+  postFeedback(content:string ,id:number,rating:number){
+    const httpOptions={
+      headers:new HttpHeaders({
+        token:sessionStorage['token']
+      })
+    }
+
+    const body={
+      content:content,
+      rating:rating
+    }
+    // console.log(`*************priceperHour=${pricePerHour}`)
+   return this.http.post(this.url+'/'+id,body,httpOptions)
+  }
+
+  getFeedback(id:number){
+    const httpOptions={
+      headers:new HttpHeaders({
+        token:sessionStorage['token']
+      })
+    }
+    return this.http.get(this.url+'/'+id,httpOptions)
+  }
+
 }
